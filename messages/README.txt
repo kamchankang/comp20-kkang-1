eca2e9eba508b2d25c2865d4d7985f45775d8b5c
Chan Kang <kamchan.kang@tufts.edu>
2017-06-19 16:02:44 -0400

    lab 9 commit 1

--- messages/README.txt
+++ messages/README.txt
@@ -1,7 +1,7 @@
-Assignment: Lab 7 (Messages, Part 1)
+Assignment: Lab 9 (Messages, Part 2)
 Name: Kam Chan Kang (Chan)
 GitHub: kamchankang
 
-1. All aspects of the work have been correctly implemented. Styling has been disregarded as suggested for part 1.
+1. All aspects of the work have been correctly implemented. Because part 1 worked, part 2 and 3 were attempted. Part 2 did not work (it shouldn't), and part 3 worked with the URI instead of the data.json file.
 2. N/A
 3. 2 hours were spent on this assignment
--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,28 @@
 // Your JavaScript goes here...
+
+var request = new XMLHttpRequest();
+
+function parse() {
+    request.open("GET", "https://messagehub.herokuapp.com/messages.json", true);
+
+    request.onreadystatechange = function() {
+	if(request.readyState == 4 && request.status == 200){
+	    console.log("Retrieved Data");
+	    data = request.responseText;
+	    console.log(data);
+	    DataArray = JSON.parse(data);
+	    var messages = document.getElementById("messages");
+	    for(i = 0; i < DataArray.length; i++){
+		messages.innerHTML += DataArray[i].content + '&nbsp' + DataArray[i].username + '<br>';
+	    }
+	}
+	else if(request.readyState == 4 && request.status != 200) {
+	    document.getElementById("messages").innerHTML = "Something wrong";
+	    
+	}
+	else {
+	    console.log ("work work work work work...");
+	}
+    };
+	request.send(null);
+}
